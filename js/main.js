d2ac69254af4841fd0fe99df046199680cdf774b
Brandon Winger-Air <brandonwinger-air@outlook.com>
2022-06-13 17:52:13 -0400

    setup api crud actions & local storage access

---
+++ js/main.js
@@ -0,0 +1,14 @@
+import KanbanAPI from "./KanbanAPI.js";
+
+// Insert
+// KanbanAPI.insertItem(2, "New Task");
+
+// Update
+// KanbanAPI.updateItem(101, {
+//     columnId: 3,
+//     position: 0,
+//     content: "Updated Task"
+// });
+
+// Delete
+// KanbanAPI.deleteItem(101);
